eabd108a8d456ec7fc85c23f97dc5895fa9d88ac
Marcin Ślusarz <marcin.slusarz@gmail.com>
2014-04-23 23:53:47 +0200

    add some ir debugging code

--- thermcam_arduino/ir.cpp
+++ thermcam_arduino/ir.cpp
@@ -42,11 +42,14 @@ bool infrared_stop_button_pressed()
   return b;
 }
 
+//static char tmpbuf[30];
 enum ir_button infrared_any_button_pressed()
 {
+//  if (!irrecv.decode(&results))
   if (!irrecv.decode(NEC, &results))
     return NONE;
 
+  //int type = results.decode_type;
   unsigned long val = results.value;
   irrecv.resume();
 
@@ -56,28 +59,52 @@ enum ir_button infrared_any_button_pressed()
     val = last_value;
 
   last_value = val;
+  
+  //sprintf(tmpbuf, _("Iir %d %lx"), type, val);
+  //Serial.println(tmpbuf);
 
   switch (val)
   {
     case 0x4BB5C03F:
+      #if TC_DEBUG > 1
+      Serial.println("left");
+      #endif
       return LEFT;
     case 0x4BB500FF:
+      #if TC_DEBUG > 1
+      Serial.println("up");
+      #endif
       return UP;
     case 0x4BB540BF:
+      #if TC_DEBUG > 1
+      Serial.println("right");
+      #endif
       return RIGHT;
     case 0x4BB5807F:
+      #if TC_DEBUG > 1
+      Serial.println("down");
+      #endif
       return DOWN;
     case 0x4BB57887:
+      #if TC_DEBUG > 1
+      Serial.println("left top");
+      #endif
       return LEFT_TOP;
     case 0x4BB538C7:
+      #if TC_DEBUG > 1
+      Serial.println("right bottom");
+      #endif
       return RIGHT_BOTTOM;
     case 0x4BB5A05F:
+      #if TC_DEBUG > 1
+      Serial.println("stop");
+      #endif
       return STOP;
     default:
       #if TC_DEBUG > 0
       if (use_serial())
       {
-        Serial.println(_("Ei1 ")); // unknown IR code
+        Serial.print(_("Ei1 "));// unknown IR code
         Serial.println(val, HEX);
       }
       #endif
